5a0839e01c2cb5d3d25dd3c9dafa60633b9d7472
AK C <zc55803903@gmail.com>
2023-02-02 02:18:51 +0800

    update

--- tests/Damus.spec.ts
+++ tests/Damus.spec.ts
@@ -9,5 +9,4 @@ test('test', async ({ page }) => {
   await page.locator('#input-editable').click();
   await page.locator('#input-editable').fill('Damus 微信交流群\n 进来互关，互相帮助学习 \nhttps://i.postimg.cc/J0NbN75B/c904082dcf3f52ec9bc336f07e02335.jpg');
   await page.getByRole('button').filter({ hasText: 'send' }).click();
-  await page.locator('.q-page > div:nth-child(3)').click();
 });
