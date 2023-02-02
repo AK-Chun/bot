26cb88552e59467140aaab74888ddd4f39321aa4
AK C <zc55803903@gmail.com>
2023-02-02 11:16:03 +0800

    upodate

---
+++ crontab4.js
@@ -0,0 +1,8 @@
+var shell = require('shelljs')
+const schedule  =  require ('node-schedule'); 
+let job = schedule.scheduleJob('*/5 * * * * *', () => {
+    if (shell.exec('npx playwright test Damus4.spec.ts').code !== 0){
+        shell.echo('failed');
+    }
+    console.log(new Date());
+});
