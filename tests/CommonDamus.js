19dafe4d706cb2f49e8ffd0f8a0414ff03d6fedd
AK C <zc55803903@gmail.com>
2023-02-02 22:21:05 +0800

    update

--- tests/CommonDamus.js
+++ tests/CommonDamus.js
@@ -5,11 +5,13 @@ const imgs = {
   "10": "https://i.postimg.cc/cH2rsmLm/f6de4fc0424b2233bf5fe660525b650.jpg",
   "11": "https://i.postimg.cc/bYmmRSFR/0fcf1eb2b65c36ed8afd0fa339b3e8c.jpg",
   "12": "https://i.postimg.cc/j2DPc2L8/91380e565cefcb39b8e3bbd05384142.jpg",
-  "13": "https://i.postimg.cc/YS8FwYFn/f525f1faf989565ba0607891dee4440.jpg"
+  "13": "https://i.postimg.cc/YS8FwYFn/f525f1faf989565ba0607891dee4440.jpg",
+  "14": "https://i.postimg.cc/43z300Mz/822f5109d9fcc1d3145d3af9893be5b.jpg",
+  "15": "https://i.postimg.cc/PJZhLvj1/01f96be5ce3155c83a4fea6c7770fbc.jpg",
+  "discord": "https://discord.gg/SusQJveN",
 };
 
 (async () => {
-  while(true) {
     const browser = await chromium.launch({
       headless: true
     });  // Or 'chromium' or 'webkit'.
@@ -25,13 +27,13 @@ const imgs = {
       await page.getByRole('button', { name: 'Close' }).click();
       flag++;
     }
-    await page.getByRole('button', { name: 'post' }).click();
-    await page.locator('#input-editable').click();
-    await page.locator('#input-editable').fill(`Damus 微信交流群\n 进来互关，互相帮助学习 \n${imgs[arguments[2]]}`);
-    await page.getByRole('button').filter({ hasText: 'send' }).click();
-    await wait(3000);
-    browser.close();
-  }
+    while(true) {
+      await page.getByRole('button', { name: 'post' }).click();
+      await page.locator('#input-editable').click();
+      await page.locator('#input-editable').fill(`Damus 官方代理中文 Discord 交流群，进群互粉，沟通更自由 \n${imgs[arguments[2]]}`);
+      await page.getByRole('button').filter({ hasText: 'send' }).click();
+      await wait(5000);
+    }
 })();
 
 function wait(ms) {
